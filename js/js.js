4e53a05d62a61797f8b2798e9d3dc6d8791c58b5
megabreakage <martin.njau@live.com>
2017-04-26 11:17:06 +0300

    adds inputs fields, button and the vague logic behind the triangle tracker

---
+++ js/js.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+
+    var a = parseInt($("input#firstLength").val());
+    var b = parseInt($("input#secondLength").val());
+    var c = parseInt($("input#thirdLength").val());
+
+    //
+    if ((a===b && a===c)){
+      //Equilateral triangle
+      $("#output").text("Equilateral Triangle");
+    }
+    else if ((a===b && a!=c) || (b===c && b!=a ) || (c===a && c!=b)) {
+      //Isoceles triangle
+      $("#output").text("Isoceles Triangle");
+    }
+    else if ((a!=b && a!==c) && (b!=a && b!=c) && (c!=a && c!==b)) {
+      //Scalene triangle
+      $("#output").text("Scalene Triangle");
+    }
+    else if ((a+b<=c) || (b+c<=a) || (c+a<=b)) {
+      //Not a triangle
+      $("#output").text("NOT a Triangle!!");
+    }
+    else {
+      $("#output").text("You've gone KooKoo!");
+    }
+
+    event.preventDefault();
+  });
+});
