a8a40a7c5bce16ec43fdb5b047ef043a4b31de2d
megabreakage <martin.njau@live.com>
2017-04-27 18:57:24 +0300

    adds background image and glass effect on form

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(event){
   $("form").submit(function(event){
 
     var a = parseInt($("input#firstLength").val());
@@ -6,7 +6,11 @@ $(document).ready(function(){
     var c = parseInt($("input#thirdLength").val());
 
     //
-    if ((a===b && a===c)){
+    if ((a+b<=c) || (b+c<=a) || (c+a<=b)) {
+      //Not a triangle
+      $("#output").text("NOT a Triangle!!");
+    }
+    else if ((a===b && a===c) && (b===c && b===a) && (c===a && c===b)){
       //Equilateral triangle
       $("#output").text("Equilateral Triangle");
     }
@@ -17,10 +21,6 @@ $(document).ready(function(){
     else if ((a!=b && a!==c) && (b!=a && b!=c) && (c!=a && c!==b)) {
       //Scalene triangle
       $("#output").text("Scalene Triangle");
-    }
-    else if ((a+b<=c) || (b+c<=a) || (c+a<=b)) {
-      //Not a triangle
-      $("#output").text("NOT a Triangle!!");
     }
     else {
       $("#output").text("You've gone KooKoo!");
